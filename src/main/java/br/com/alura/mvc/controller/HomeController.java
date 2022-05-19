26a97a36f6e5c1e3b92b9bb60d2c6d93b8e53f74
unknown <edwanestudos@gmail.com>
2022-05-19 17:45:11 -0300

    Mapeando entidades

--- src/main/java/br/com/alura/mvc/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/controller/HomeController.java
@@ -1,9 +1,12 @@
 package br.com.alura.mvc.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,15 +16,17 @@ import br.com.alura.mvc.model.Pedido;
 
 @Controller
 public class HomeController {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Apple iPhone 13 Pro (256 GB) - Dourado");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51y+xXlXPrL._AC_SX679_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Apple-iPhone-13-Pro-256-GB/dp/B09L1NQ9R1?ref_=Oct_DLandingS_D_880408ea_60&smid=A122ZHUG7SODV0&th=1");
-		pedido.setDescricao("Tela Super Retina XDR de 6,1 polegadas com ProMotion para uma sensação mais rápida e responsiva");
 		
-		List<Pedido>pedidos = Arrays.asList(pedido);
+		String query = "select p from Pedido p";
+		List<Pedido> pedidos = entityManager.createQuery(query, Pedido.class).getResultList();
+		
+		
+		
 		model.addAttribute("pedidos", pedidos);
 		return"home";
 	}
